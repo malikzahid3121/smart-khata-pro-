b6c7d5f46a19ac040a5e4d332d70e55a74bebfa6
malikzahid3121 <malikzahid7754172@gmail.com>
2026-07-11 01:38:57 +0400

    Update CustomerListScreen.js

--- screens/CustomerListScreen.js
+++ screens/CustomerListScreen.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { getCustomers } from '../database/customerStorage';
 
 export default function CustomerListScreen() {
 
-  const customers = [
-    { id: '1', name: 'Example Customer', balance: '0' }
-  ];
+  const [customers, setCustomers] = useState([]);
+
+  useEffect(() => {
+    loadCustomers();
+  }, []);
+
+  const loadCustomers = async () => {
+    const data = await getCustomers();
+    setCustomers(data);
+  };
+
 
   return (
     <View style={styles.container}>
@@ -17,16 +26,18 @@ export default function CustomerListScreen() {
       <FlatList
         data={customers}
         keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
+        renderItem={({item}) => (
+
           <View style={styles.card}>
             <Text style={styles.name}>
               {item.name}
             </Text>
 
             <Text>
-              Balance: {item.balance}
+              Balance: Rs {item.balance}
             </Text>
           </View>
+
         )}
       />
 
@@ -34,30 +45,31 @@ export default function CustomerListScreen() {
   );
 }
 
+
 const styles = StyleSheet.create({
 
-  container: {
-    flex: 1,
-    padding: 20,
+  container:{
+    flex:1,
+    padding:20
   },
 
-  title: {
-    fontSize: 28,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    marginBottom: 20,
+  title:{
+    fontSize:28,
+    fontWeight:'bold',
+    textAlign:'center',
+    marginBottom:20
   },
 
-  card: {
-    padding: 15,
-    borderWidth: 1,
-    borderRadius: 10,
-    marginBottom: 10,
+  card:{
+    padding:15,
+    borderWidth:1,
+    borderRadius:10,
+    marginBottom:10
   },
 
-  name: {
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
+  name:{
+    fontSize:18,
+    fontWeight:'bold'
+  }
 
 });
