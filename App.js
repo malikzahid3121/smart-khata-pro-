1b7f1990e46b88d7d89a82a5fb7e0852d82ebfa9
malikzahid3121 <malikzahid7754172@gmail.com>
2026-07-11 01:26:06 +0400

    Update App.js

--- App.js
+++ App.js
@@ -1,30 +1,8 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import Navigation from './Navigation';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Smart Khata Pro</Text>
-      <Text style={styles.subtitle}>
-        Digital Khata App
-      </Text>
-    </View>
+    <Navigation />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#ffffff',
-  },
-  title: {
-    fontSize: 28,
-    fontWeight: 'bold',
-  },
-  subtitle: {
-    fontSize: 18,
-    marginTop: 10,
-  },
-});
