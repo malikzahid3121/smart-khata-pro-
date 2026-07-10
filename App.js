4fca74873604f7f3bbc3d38e46ff656703ae57ea
malikzahid3121 <malikzahid7754172@gmail.com>
2026-07-11 01:20:22 +0400

    Create App.js

---
+++ App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Smart Khata Pro</Text>
+      <Text style={styles.subtitle}>
+        Digital Khata App
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ffffff',
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+  },
+  subtitle: {
+    fontSize: 18,
+    marginTop: 10,
+  },
+});
