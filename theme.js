8f77eff719a14d095c214ed672a9ab57c2706f3e
malikzahid3121 <malikzahid7754172@gmail.com>
2026-07-11 01:32:18 +0400

    Create theme.js

---
+++ theme.js
@@ -0,0 +1,9 @@
+export const Colors = {
+  primary: '#1E88E5',
+  secondary: '#43A047',
+  background: '#FFFFFF',
+  text: '#222222',
+  card: '#F5F5F5',
+};
+
+export const AppName = "Smart Khata Pro";
