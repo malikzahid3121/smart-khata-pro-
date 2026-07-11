798d733659ddeaf98c319dd1114d346118c2eb6a
malikzahid3121 <malikzahid7754172@gmail.com>
2026-07-11 20:13:57 +0400

    Update TransactionListScreen.js

--- screens/TransactionListScreen.js
+++ screens/TransactionListScreen.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, FlatList, StyleSheet } from 'react-native';
-import { getTransactions } from '../services/transactionService';
+import { View, Text, FlatList, StyleSheet, Button } from 'react-native';
+import { getTransactions, deleteTransaction } from '../services/transactionService';
 
 export default function TransactionListScreen() {
 
@@ -10,38 +10,71 @@ export default function TransactionListScreen() {
     loadTransactions();
   }, []);
 
+
   const loadTransactions = async () => {
     const data = await getTransactions();
-    setTransactions(data);
+    setTransactions(data || []);
   };
+
+
+  const removeTransaction = async (id) => {
+
+    await deleteTransaction(id);
+
+    loadTransactions();
+
+  };
+
 
   return (
     <View style={styles.container}>
 
       <Text style={styles.title}>
-        Transactions
+        📋 Transactions
       </Text>
 
+
       <FlatList
+
         data={transactions}
+
         keyExtractor={(item) => item.id.toString()}
+
+
+        ListEmptyComponent={
+          <Text style={styles.empty}>
+            No Transactions Found
+          </Text>
+        }
+
+
         renderItem={({item}) => (
+
           <View style={styles.card}>
 
-            <Text>
+            <Text style={styles.name}>
               {item.title}
             </Text>
 
             <Text>
-              {item.amount}
+              Amount: {item.amount}
             </Text>
 
             <Text>
-              {item.type}
+              Type: {item.type}
             </Text>
 
+
+            <Button
+              title="🗑️ Delete"
+              onPress={() => removeTransaction(item.id)}
+            />
+
+
           </View>
+
         )}
+
       />
 
     </View>
@@ -49,24 +82,40 @@ export default function TransactionListScreen() {
 }
 
 
+
 const styles = StyleSheet.create({
 
   container:{
     flex:1,
-    padding:20
+    padding:20,
+    backgroundColor:'#fff'
   },
 
+
   title:{
-    fontSize:24,
+    fontSize:28,
     fontWeight:'bold',
     marginBottom:20
   },
+
 
   card:{
     padding:15,
     marginBottom:10,
     backgroundColor:'#eee',
     borderRadius:10
+  },
+
+
+  name:{
+    fontSize:18,
+    fontWeight:'bold'
+  },
+
+
+  empty:{
+    textAlign:'center',
+    marginTop:50
   }
 
 });
